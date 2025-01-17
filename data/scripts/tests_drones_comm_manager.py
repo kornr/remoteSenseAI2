981e7f3312b3208dbe9711e68a45c69b9ea6189e
kornr <infotecnova6@gmail.com>
2025-01-17 08:46:38 +0100

    Update tests_drones_comm_manager.py

--- data/scripts/tests_drones_comm_manager.py
+++ data/scripts/tests_drones_comm_manager.py
@@ -1,5 +1,8 @@
-import pytest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from data.scripts.test_drones_comm_manager import DroneCommManager
+
 
 @pytest.fixture
 def test_drones_comm_manager():
